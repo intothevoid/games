56a9b24e1da17edbdce2af206a477d728b3d63f8
Karan Kadam <karank@outlook.com>
2021-01-26 16:20:40 +1030

    added memorise - cs193 lesson 1

---
+++ memorise/memorise/memoriseApp.swift
@@ -0,0 +1,17 @@
+//
+//  memoriseApp.swift
+//  memorise
+//
+//  Created by Karan Kadam on 26/1/21.
+//
+
+import SwiftUI
+
+@main
+struct memoriseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
